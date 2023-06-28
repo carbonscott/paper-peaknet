2a11322b87b46c477f6be170e9d760a0cc1a39a7
carbonscott <wangimagine@gmail.com>
2023-06-28 14:19:01 -0700

    Add packaging scripts.

---
+++ package.arxiv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tar czvf                             \
+    ./PeakNet.tar.gz                 \
+    ./bibliography.bib               \
+    ./figures/peaknet_steps.pdf      \
+    ./figures/network_arc.pdf        \
+    ./figures/data_aug.pdf           \
+    ./figures/data_engine.pdf        \
+    ./figures/automask.pdf           \
+    ./figures/automask.agbehnate.pdf \
+    ./manuscript.tex                 \
+    ./style.sty                      \
+    ./more.sty                       \
+    ./constants.sty                  \
+    ./manuscript.bbl
---
+++ package.natcomm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tar czvf                             \
+    ./PeakNet.natcomm.tar.gz         \
+    ./figures/peaknet_steps.pdf      \
+    ./figures/network_arc.pdf        \
+    ./figures/data_aug.pdf           \
+    ./figures/data_engine.pdf        \
+    ./figures/automask.pdf           \
+    ./figures/automask.agbehnate.pdf \
+    ./manuscript.natcomm.tex         \
+    ./manuscript.natcomm.pdf
